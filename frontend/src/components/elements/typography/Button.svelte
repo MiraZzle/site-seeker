88cd65229f42f376ed64ec5f09a5e9757a2455a2
Matěj Foukal <matfou01@gmail.com>
2024-08-08 17:53:03 +0200

    feat: define components

---
+++ frontend/src/components/elements/typography/Button.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+    export let type: 'primary' | 'secondary' | 'dark' = 'primary';
+    let btnType = `btn_${type}`;
+</script>
+
+<button class="btn {btnType}">
+    <slot></slot>
+</button>
+
+<style lang="scss">
+    .btn {
+        display: flex;
+        border-radius: 8px;
+        padding: 12px;
+        border: none;
+        gap: 8px;
+
+        &_primary {
+            background-color: #007bff;
+            color: #fff;
+        }
+        &_secondary {
+            background-color: #6c757d;
+            color: #fff;
+        }
+        &_dark {
+            background-color: #343a40;
+            color: #fff;
+        }
+    }
+</style>
---
+++ frontend/src/routes/components/button/+page.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+    import Button from "../../../components/elements/typography/Button.svelte";
+    import ExploreIcon from "../../../components/utils/icons/ExploreIcon.svelte";
+</script>
+
+<Button type="primary"><ExploreIcon></ExploreIcon>Explore</Button>
