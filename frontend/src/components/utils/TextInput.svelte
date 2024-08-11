5f08932c5097093e7ad50660d5bc7e997f4cb387
Matěj Foukal <matfou01@gmail.com>
2024-08-11 22:48:06 +0200

    feat: text input

---
+++ frontend/src/components/utils/TextInput.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+    export let value = '';
+    export let id = '';
+    export let placeholder = 'Value';
+    export let description = 'Label';
+    export let type : 'text' | 'password' | 'number' = 'text';
+</script>
+
+<div class="input-block">
+<label for={id} class="input-block__label">
+    {description}
+</label>
+<input id={id} type={type} value={value} placeholder={placeholder} class="input-block__input" />
+</div>
+
+<style lang="scss">
+    @import "../../styles/variables.scss";
+
+    .input-block {
+        display: flex;
+        flex-direction: column;
+        gap: 8px;
+        &__label {
+            font-size: 16px;
+            line-height: 140%;
+            font-weight: 400;
+            color: $c-black;
+        }
+        &__input {
+            border: 1px solid $c-gray-outline;
+            border-radius: 8px;
+            padding: 12px;
+            width: 100%;
+        }
+    }
+</style>
---
+++ frontend/src/routes/components/text_input/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import TextInput from "$components/utils/TextInput.svelte";
+</script>
+
+<div class="text-input-container">
+    <TextInput description="Test1" placeholder="aaa" />
+</div>
+
+<style lang="scss">
+    .text-input-container {
+        display: flex;
+        margin: 16px;
+    }
+</style>
