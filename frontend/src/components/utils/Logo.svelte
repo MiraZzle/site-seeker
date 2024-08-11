52abc8afe74c3d8e711dd62ccc31264c829fe9b5
Matěj Foukal <matfou01@gmail.com>
2024-08-11 14:25:54 +0200

    feat: logo

---
+++ frontend/src/components/utils/Logo.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import Icon from "$components/utils/icons/Icon.svelte";
+</script>
+
+<a href="#/" class="logo-container">
+  <Icon />
+  <span class="logo-name">SiteSeeker</span>
+</a>
+
+<style lang="scss">
+  @import "../../styles/variables.scss";
+
+  .logo-container {
+    display: flex;
+    align-items: center;
+    gap: 6px;
+
+    .logo-name {
+      font-size: 24px;
+      line-height: 120%;
+      letter-spacing: -0.5px;
+      font-weight: 600;
+      text-decoration: none;
+      color: $c-black;
+    }
+  }
+</style>
