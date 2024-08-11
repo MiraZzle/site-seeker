5cbb0cf8ce32e7f1f7b4c5dc0f94cc3f7682bf57
Matěj Foukal <matfou01@gmail.com>
2024-08-11 22:46:42 +0200

    feat: checkbox input

---
+++ frontend/src/routes/components/checkbox/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import Checkbox from "$components/utils/Checkbox.svelte";
+</script>
+
+<div class="checkbox-container">
+    <Checkbox label="Test1" />
+</div>
+
+<style lang="scss">
+    .checkbox-container {
+        display: flex;
+        margin: 16px;
+    }
+</style>
