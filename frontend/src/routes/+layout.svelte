c031a1a4b680a74aea9989812a5fe5acdb5ac8e5
Matěj Foukal <matfou01@gmail.com>
2024-08-11 15:13:45 +0200

    doc: layout note

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,5 +1,12 @@
 <script lang="ts">
     import '$styles/global.scss';
+
+    /*
+    PRO BUDOUCI GENERACE:
+    dolu pridame pak automaticky loading navbaru a footeru
+    zatim to vynechame kvuli testovani
+    */
 </script>
+
 
 <slot />
