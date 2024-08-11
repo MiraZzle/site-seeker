eb243a1d580a8c57fe5d696ee45511f835e30d1a
Matěj Foukal <matfou01@gmail.com>
2024-08-11 22:47:11 +0200

    feat: tag button input

---
+++ frontend/src/components/utils/TagButton.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import Button from "$components/elements/typography/Button.svelte";
+    import Plus from "./icons/Plus.svelte";
+
+    function addTag() {
+        console.log("Tag added");
+    }
+</script>
+
+<Button type="dark" action={addTag}> Add Tag <Plus/> </Button>
---
+++ frontend/src/routes/components/tag_button/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import TagButton from "$components/utils/TagButton.svelte";
+</script>
+
+<div class="tag-button-container">
+    <TagButton />
+</div>
+
+<style lang="scss">
+    .tag-button-container {
+        display: flex;
+        margin: 16px;
+    }
+</style>
