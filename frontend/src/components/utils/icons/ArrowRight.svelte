07eecf1a0c5217d8f13ab6fb541ab8ab0ccbf9a1
Matěj Foukal <matfou01@gmail.com>
2024-08-13 22:32:06 +0200

    feat: new icons

---
+++ frontend/src/components/utils/icons/ArrowRight.svelte
@@ -0,0 +1,3 @@
+<svg width="11" height="12" viewBox="0 0 11 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M0.833374 5.99998H10.1667M10.1667 5.99998L5.50004 1.33331M10.1667 5.99998L5.50004 10.6666" stroke="#1E1E1E" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
