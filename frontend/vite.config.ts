fed75658638870e3e4e0a9fe455530b83f394c81
Matěj Foukal <matfou01@gmail.com>
2024-09-24 23:23:00 +0200

    feat: sveltekit dockerfile [working version]

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [sveltekit()],
+  build: {
+    outDir: "./build"
+  },
   css: {
     preprocessorOptions: {
       scss: {
